e55ee130f3c8a793a42b111788ee728d682f0dc7
dk <khoaguin>
2021-10-12 13:02:47 +0300

    done forward pass

--- notebooks/split2ConvHE/client.py
+++ notebooks/split2ConvHE/client.py
@@ -4,15 +4,17 @@ import socket
 import struct
 import time
 from pathlib import Path
-from typing import List
-import threading
+from typing import List, Union
+import math
 
 import h5py
 import numpy as np
 import tenseal as ts
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from icecream import ic
+ic.configureOutput(includeContext=True)
 from tenseal.enc_context import Context
 from tenseal.tensors.ckkstensor import CKKSTensor
 from tenseal.tensors.ckksvector import CKKSVector
@@ -20,26 +22,22 @@ from tenseal.tensors.plaintensor import PlainTensor
 from torch.optim import SGD, Adam
 from torch.utils.data import DataLoader, Dataset
 
-
 print(f'torch version: {torch.__version__}')
 print(f'tenseal version: {ts.__version__}')
 
 project_path = Path.cwd().parent.parent
 print(f'project_path: {project_path}')
 
-# some global variables
-host = 'localhost'
-port = 10080
-max_recv = 4096
 
-dry_run = True # break after 2 batches for 2 epoch, set batch size to be 2
-if dry_run:
-    batch_size = 2
-    epoch = 2
-else:
-    batch_size = 32
-    epoch = 400
-total_batch = 13245/batch_size
+def recvall(sock, n) -> Union[None, bytes]:
+    # helper function to receive n bytes or return None if EOF is hit
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
 
 
 class ECG(Dataset):
@@ -65,50 +63,265 @@ class ECG(Dataset):
         return torch.tensor(self.x[idx], dtype=torch.float), torch.tensor(self.y[idx])
 
 
+class EcgClient(nn.Module):
+    """The client's 1D CNN model
+
+    Args:
+        nn ([torch.Module]): [description]
+    """
+    def __init__(self, context: Context, init_weight_path: Union[str, Path]):
+        super(EcgClient, self).__init__()
+        self.conv1 = nn.Conv1d(1, 16, 7, padding=3)  # 128 x 16
+        self.relu1 = nn.LeakyReLU()
+        self.pool1 = nn.MaxPool1d(2)  # 64 x 16
+        self.conv2 = nn.Conv1d(16, 16, 5, padding=2)  # 64 x 16
+        self.relu2 = nn.LeakyReLU()
+        self.pool2 = nn.MaxPool1d(2)  # 32 x 16
+        
+        self.load_init_weights(init_weight_path)
+        self.context = context
+    
+    def forward(self, x: torch.Tensor) -> CKKSTensor:
+        x = self.conv1(x)  # [batch_size, 16, 128]
+        x = self.relu1(x)
+        x = self.pool1(x)  # [batch_size, 16, 64]
+        x = self.conv2(x)  # [batch_size, 16, 64]
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = x.view(-1, 32 * 16)  # [batch_size, 16, 32]
+        enc_x: CKKSTensor = ts.ckks_tensor(self.context, x.tolist())  # [batch_size, 512]
+        return enc_x
+    
+    def load_init_weights(self, init_weight_path: Union[str, Path]):
+        checkpoint = torch.load(init_weight_path)
+        self.conv1.weight.data = checkpoint["conv1.weight"]
+        self.conv1.bias.data = checkpoint["conv1.bias"]
+        self.conv2.weight.data = checkpoint["conv2.weight"]
+        self.conv2.bias.data = checkpoint["conv2.bias"]
+
+    def backward(self):
+        """ 
+        Calculates the gradients
+        """
+        raise NotImplementedError
+
+    def update_params(self):
+        """
+        Update the parameters based on the gradients calculated in backward()
+        """
+        raise NotImplementedError
+
+
 class Client: 
     """
     The class that represents the client in the protocol.    
     """    
     def __init__(self) -> None:
         # paths to files and directories
-        self.data_dir = 'data'  # used to be 'mitdb'
-        self.train_name = 'train_ecg.hdf5'
-        self.test_name = 'test_ecg.hdf5'
+        self.socket = None
+        self.train_loader = None
+        self.test_loader = None
+        self.context = None
+        self.ecg_model = None
+        self.device = None
 
-    def load_ecg_dataset(self) -> None:
-        train_dataset = ECG(self.data_dir, self.train_name, self.test_name, train=True)
-        test_dataset = ECG(self.data_dir, self.train_name, self.test_name, train=False)
+    def load_ecg_dataset(self, data_dir: str, 
+                            train_name: str, test_name: str,
+                            batch_size: int) -> None:
+        """[summary]
+
+        Args:
+            data_dir (str): [description]
+            train_name (str): [description]
+            test_name (str): [description]
+            batch_size (int): [description]
+        """
+        train_dataset = ECG(data_dir, train_name, test_name, train=True)
+        test_dataset = ECG(data_dir, train_name, test_name, train=False)
         self.train_loader = DataLoader(train_dataset, batch_size=batch_size)
         self.test_loader = DataLoader(test_dataset, batch_size=batch_size)
         
-    def init_socket(self, host, port):
+    def init_socket(self, host, port) -> None:
         """[summary]
 
         Args:
             host ([str]): [description]
             port ([int]): [description]
         """
-        client_socket = socket.socket()
-        client_socket.connect((host, port))  # connect to a remote [server] address,
+        self.socket = socket.socket()
+        self.socket.connect((host, port))  # connect to a remote [server] address,
 
-    def send_msg(sock, msg):
+    def send_msg(self, msg) -> None:
         # prefix each message with a 4-byte length in network byte order
         msg = struct.pack('>I', len(msg)) + msg
-        sock.sendall(msg)
+        self.socket.sendall(msg)
+
+    def recv_msg(self):
+        # read message length and unpack it into an integer
+        raw_msglen = recvall(self.socket, 4)
+        if not raw_msglen:
+            return None
+        msglen = struct.unpack('>I', raw_msglen)[0]
+        # read the message data
+        data: bytes = recvall(self.socket, msglen)
+        return data
+
+    def make_tenseal_context(self, poly_modulus_degree: int,
+                                coeff_mod_bit_sizes: List[int], 
+                                global_scale: int) -> Context:
+        """Generate the TenSeal context to encrypt the activation maps
+
+        Args:
+            poly_modulus_degree (int): [description]
+            coeff_mod_bit_sizes (List[int]): [description]
+            global_scale (int): [description]
+
+        Returns:
+            Context: [description]
+        """
+        self.context = ts.context(
+            ts.SCHEME_TYPE.CKKS, 
+            poly_modulus_degree=poly_modulus_degree, 
+            coeff_mod_bit_sizes=coeff_mod_bit_sizes
+        )
+        self.context.global_scale = global_scale
+        # self.context.generate_galois_keys()
+    
+    def send_context(self) -> None:
+        """Send the context to the server
+        """
+        self.send_msg(self.context.serialize(save_secret_key=False))
+
+    def build_model(self, init_weight_path: Union[str, Path]) -> None:
+        """Build the neural network model for the client
+
+        Raises:
+            TypeError: if the tenseal context needed to encrypt the activation 
+                        map is None, then raise an error
+        """
+        if self.context == None:
+            raise TypeError("tenseal context is None")
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        print(f'client device: {torch.cuda.get_device_name(0)}')
+        self.ecg_model = EcgClient(context=self.context, 
+                                    init_weight_path=init_weight_path)
+        self.ecg_model.to(self.device)
+
+    def set_random_seed(self, seed: int) -> None:
+        """Setting the random seed for the training process
+
+        Args:
+            seed (int): [description]
+        """
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    def train(self, epoch: int, total_batch: int):
+        if self.ecg_model == None:
+            raise TypeError("client's model has not been constructed yet")
+        train_losses = list()
+        train_accs = list()
+        test_losses = list()
+        test_accs = list()
+        best_test_acc = 0  # best test accuracy
+        loss_func = nn.CrossEntropyLoss()
+
+        for e in range(epoch):
+            print(f"Epoch {e+1} -----")
+            epoch_train_loss = 0.0
+            epoch_correct, epoch_total = 0, 0
+
+            # training loop
+            for i, batch in enumerate(self.train_loader):
+                # --- Forward Pass ---
+                x, y = batch  # get the input data and ground-truth output in the batch
+                x, y = x.to(self.device), y.to(self.device)  # put to cuda or cpu
+                he_a: CKKSTensor = self.ecg_model(x)  # [batch_size, 512]
+                print(f'Encrypted activations (he_a): {he_a}')
+                # converting the HE encrypted activation maps into byte stream
+                he_a_bytes: bytes = he_a.serialize() 
+                # the client sends the byte streams to the server
+                print("\U0001F601 Sending he_a to the server")
+                self.send_msg(msg=he_a_bytes)
+                # the client receives the encrypted activations after 2 linear layers 
+                # from the server
+                he_a2_bytes: bytes = self.recv_msg()
+                print("\U0001F602 Received he_a2 from the server")
+                he_a2: CKKSTensor = CKKSTensor.load(context=self.context,
+                                                    data=he_a2_bytes)
+                # the client decrypts he_a2 and apply softmax to get the predictions
+                a2: List = he_a2.decrypt().tolist() # the client decrypts he_a2
+                print(f"Decrypted activations: {a2}")
+                y_hat: torch.Tensor = F.softmax(torch.tensor(a2), dim=1)  # apply softmax
+                y_hat = y_hat.to(self.device)
+                print(f'y_hat: {y_hat}')
+                # the client calculates the training loss and accuracy
+                batch_loss: torch.Tensor = loss_func(y_hat, y)
+                epoch_train_loss += batch_loss.item()
+                epoch_correct += torch.sum(y_hat.argmax(dim=1) == y).item()
+                epoch_total += len(y)
+
+                debug = 1
+                # --- Backward pass ---
+
+                if i == total_batch-1: break
+            
+            # save the average training losses and accuracies over all batches for each epoch
+            train_losses.append(epoch_train_loss / total_batch)
+            train_accs.append(epoch_correct / epoch_total)
+
+    def test():
+        pass
 
 
-if __name__ == '__main__':
+def main(hyperparams):
     client = Client()
     client.init_socket(host, port)
+    # sending hyperparameters to the server
+    ic(hyperparams)
+    print("Sending hyperparams to the server")
+    client.send_msg(msg=pickle.dumps(hyperparams))
+    client.load_ecg_dataset('data', 'train_ecg.hdf5', 'test_ecg.hdf5', 
+                            hyperparams['batch_size'])
+    client.make_tenseal_context(8192, 
+                                [40, 21, 21, 21, 21, 21, 21, 40], 
+                                pow(2, 21))
+    print("Sending the context to the server")
+    client.send_context()
+    client.build_model('init_weight.pth')
+    client.set_random_seed(hyperparams['seed'])
+    client.train(epoch=hyperparams['epoch'], 
+                 total_batch=hyperparams['total_batch'])
+
     
+if __name__ == '__main__':
+    # some global variables
+    host = 'localhost'
+    port = 10080
+    max_recv = 4096
+    
+    dry_run = True # break after 2 batches for 2 epoch, set batch size to be 2
+    if dry_run:
+        batch_size = 2
+        epoch = 2
+        total_batch = 2
+    else:
+        batch_size = 32
+        epoch = 400
+        total_batch = math.ceil(13245/batch_size)
+    lr = 0.001
+    seed = 0
 
+    hyperparams = {
+        'batch_size': batch_size,
+        'total_batch': total_batch,
+        'epoch': epoch,
+        'lr': lr,
+        'seed': seed
+    }
 
-
-
-
-
-
-
-
-
-
+    main(hyperparams)
--- notebooks/split2ConvHE/server.py
+++ notebooks/split2ConvHE/server.py
@@ -4,26 +4,86 @@ import socket
 import struct
 import time
 from pathlib import Path
-from typing import List
+from typing import List, Union
 
 import h5py
 import numpy as np
 import tenseal as ts
 import torch
+from torch.cuda import _sleep
 import torch.nn as nn
 from icecream import ic
+ic.configureOutput(includeContext=True)
 from tenseal.enc_context import Context
 from tenseal.tensors.ckkstensor import CKKSTensor
 from tenseal.tensors.ckksvector import CKKSVector
 from tenseal.tensors.plaintensor import PlainTensor
 from torch.optim import SGD, Adam
 from torch.utils.data import DataLoader, Dataset
+print(f'tenseal version: {ts.__version__}')
+print(f'torch version: {torch.__version__}')
 
 
-# some global variables
-host = 'localhost'
-port = 10080
-max_recv = 4096
+def recvall(sock, n) -> Union[None, bytes]:
+    # helper function to receive n bytes or return None if EOF is hit
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+class EcgServer(nn.Module):
+    def __init__(self, init_weight_path: Union[str, Path]):
+        """Constructor
+
+        Args:
+            init_weight (str): the string path to the initial weight
+        """
+        super(EcgServer, self).__init__()
+        checkpoint = torch.load(init_weight_path)
+        self.linear3_weight: torch.Tensor = checkpoint["linear3.weight"]  # [128, 512]
+        self.linear3_bias: torch.Tensor = checkpoint["linear3.bias"]  # [128]
+        self.linear4_weight: torch.Tensor = checkpoint["linear4.weight"]  # [5, 128]
+        self.linear4_bias: torch.Tensor = checkpoint["linear4.bias"]  # [5]
+
+    def enc_linear(self, 
+                    enc_x: CKKSTensor, 
+                    W: torch.Tensor, 
+                    b: torch.Tensor) -> CKKSTensor:
+        """
+        The linear layer on homomorphic encrypted data
+        Based on https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
+        """
+        Wt = torch.transpose(W, 0, 1)
+        return enc_x.mm(Wt) + b
+
+    @staticmethod
+    def approx_leaky_relu(enc_x: CKKSTensor):
+        # 2.30556314780491e-19*x**5 - 0.000250098672095587*x**4 - 
+        # 2.83384427035571e-17*x**3 + 0.0654264479654812*x**2 + 
+        # 0.505000000000001*x + 0.854102848838318
+        return enc_x.polyval([0.854, 0.505, 0.0654])
+
+    def forward(self, enc_x: CKKSTensor) -> CKKSTensor:
+        x = self.enc_linear(enc_x, self.linear3_weight, self.linear3_bias)  # [batch_size, 128]
+        x = EcgServer.approx_leaky_relu(x)
+        x = self.enc_linear(x, self.linear4_weight, self.linear4_bias)  # [batch_size, 5]
+        return x
+
+    def backward(self):
+        """ 
+        Calculates the gradients
+        """
+        raise NotImplementedError
+
+    def update_params(self):
+        """
+        Update the parameters based on the gradients calculated in backward()
+        """
+        raise NotImplementedError
 
 
 class Server:
@@ -31,7 +91,10 @@ class Server:
     The class that represents the server in the protocol
     """
     def __init__(self) -> None:
-        pass
+        self.socket = None
+        self.device = None
+        self.ecg_model = None
+        self.client_ctx = None
 
     def init_socket(self, host, port):
         """[summary]
@@ -40,14 +103,105 @@ class Server:
             host ([str]): [description]
             port ([int]): [description]
         """
-        server_socket = socket.socket()
-        server_socket.bind((host, port))  # associates the socket with its local address
-        server_socket.listen()
+        self.socket = socket.socket()
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((host, port))  # associates the socket with its local address
+        self.socket.listen()
         print('Listening on', (host, port))
-        conn, addr = server_socket.accept()  # wait for the client to connect
-        print(f'Connection: {conn} \nAddress: {addr}')
+        self.socket, addr = self.socket.accept()  # wait for the client to connect
+        print(f'Connection: {self.socket} \nAddress: {addr}')
+
+    def recv_msg(self):
+        # read message length and unpack it into an integer
+        raw_msglen = recvall(self.socket, 4)
+        if not raw_msglen:
+            return None
+        msglen = struct.unpack('>I', raw_msglen)[0]
+        # read the message data
+        data: bytes = recvall(self.socket, msglen)
+        return data
+
+    def recv_ctx(self):
+        client_ctx_bytes: bytes = self.recv_msg()
+        self.client_ctx: Context = Context.load(client_ctx_bytes)
+
+    def send_msg(self, msg) -> None:
+        # prefix each message with a 4-byte length in network byte order
+        msg = struct.pack('>I', len(msg)) + msg
+        self.socket.sendall(msg)
+
+    def build_model(self, init_weight_path: Union[str, Path]) -> None:
+        """Build the neural network model for the server
+        """
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        print(f'server device: {torch.cuda.get_device_name(0)}')
+        self.ecg_model = EcgServer(init_weight_path)
+        # self.ecg_model.to(self.device)
+
+    def set_random_seed(self, seed: int) -> None:
+        """Setting the random seed for the training process
+
+        Args:
+            seed (int): [description]
+        """
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    def train(self, epoch: int, total_batch: int):
+        train_losses = list()
+        train_accs = list()
+        test_losses = list()
+        test_accs = list()
+        best_test_acc = 0  # best test accuracy
+
+        for e in range(epoch):
+            print(f"Epoch {e+1} -----")
+            
+            train_loss = 0.0
+            correct, total = 0, 0
+            for i in range(total_batch):
+                # --- Forward Pass ---
+                # receive the encrypted activation maps from the client
+                he_a_bytes: bytes = self.recv_msg()
+                print("\U0001F601 Received he_a from the client")
+                he_a: CKKSTensor = CKKSTensor.load(context=self.client_ctx,
+                                                   data=he_a_bytes)
+                # the server puts the encrypted activations 
+                # through 2 linear layers and send the outputs to the client
+                he_a2: CKKSTensor = self.ecg_model(he_a)
+                print(f"Server's outputs (he_a2): {he_a2}")
+                print("\U0001F602 Sending he_a2 to the client")
+                self.send_msg(msg=he_a2.serialize())
+                
+                # --- Backward pass ---
+                debug = 1
+
+
+def main():
+    server = Server()
+    server.init_socket(host, port)
+    # receiving the hyperparameters from the clients
+    hyperparams = server.recv_msg()
+    print("Receiving hyperparams from the client")
+    hyperparams = pickle.loads(hyperparams)
+    ic(hyperparams)
+    server.build_model('init_weight.pth')
+    server.set_random_seed(seed=hyperparams['seed'])
+    # before training, receive the TenSeal context (without the secret key) from the client
+    server.recv_ctx()
+    print("Received the tenseal context from the client")
+    server.train(epoch=hyperparams['epoch'], total_batch=hyperparams['total_batch'])
 
 
 if __name__ == '__main__':
-    server = Server()
-    server.init_socket(host, port)
+    # some global variables
+    host = 'localhost'
+    port = 10080
+    max_recv = 4096
+
+    main()
+    
